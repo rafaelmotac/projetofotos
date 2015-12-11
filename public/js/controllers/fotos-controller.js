94af6ae3ca851664386852db9c118b94f9b3b221
Rafael Correia <rafaelmcdono@gmail.com>
2015-12-11 16:03:13 -0300

    primeiro commit

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,17 @@
+angular.module('alurapic').controller('FotosController',function($scope){
+
+	$scope.fotos = [
+		{
+			titulo: 'Leão 1',
+			url: 'img/leao.jpg'
+		},
+		{
+			titulo: 'Leão 2',
+			url: 'img/leao.jpg'
+		},
+		{
+			titulo: 'Leão 3',
+			url: 'img/leao.jpg'
+		}
+	];
+});
